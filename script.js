bbcd0808487aa62126f9ffbf1611bbdf23b7db21
vaibhav1491 <102009018+vaibhav1491@users.noreply.github.com>
2023-10-05 22:38:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-//TMDB 
-
 const API_KEY = 'api_key=1cf50e6248dc270629e802686245c2c8';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
